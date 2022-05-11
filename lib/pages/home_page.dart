8516350fe7aa890cce52c6db73fecaac845f5f84
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2022-05-10 22:57:55 -0500

    Futures 3

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,23 +1,20 @@
-
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
-
   @override
   State<HomePage> createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-
   String nombre = "No hay nombre";
 
   @override
-  initState(){
+  initState() {
     super.initState();
     estaFuncionEsParaObtenerLaData();
   }
 
-  estaFuncionEsParaObtenerLaData() async{
+  estaFuncionEsParaObtenerLaData() async {
     // getName().then((value){
     //   nombre = value;
     //   setState(() {
@@ -26,44 +23,52 @@ class _HomePageState extends State<HomePage> {
     // });
 
     nombre = await getName();
-    setState(() {
-    });
+    setState(() {});
   }
 
   Future<String> getName() async {
-    return Future.delayed(Duration(seconds: 3),(){
+    return Future.delayed(Duration(seconds: 3), () {
       return "Manolo Robles";
     });
   }
 
   Future<List<String>> getProducts() async {
-    return Future.delayed(Duration(seconds: 3),(){
-      return [
-        "Corbatas",
-        "Camisas",
-        "Polos",
-        "Sacos"
-      ];
+    print("Estoy en el método getProducts");
+    return Future.delayed(Duration(seconds: 3), () {
+      return ["Corbatas", "Camisas", "Polos", "Sacos"];
     });
   }
-
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: FutureBuilder(
         future: getProducts(),
-        builder: (BuildContext context, AsyncSnapshot snapshot){
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
 
-          if(snapshot.hasData){
+          print(snapshot.connectionState);
+          print(snapshot.hasData);
+          print(snapshot.data);
+
+          if (snapshot.hasData) {
             List<String> lista = snapshot.data;
-            return ListView.builder(
-              itemCount: lista.length,
-              itemBuilder: (BuildContext context, int index){
-                return ListTile(
-                  title: Text(lista[index]),
-                );
-              },
+            // return ListView.builder(
+            //   itemCount: lista.length,
+            //   itemBuilder: (BuildContext context, int index){
+            //     return ListTile(
+            //       title: Text(lista[index]),
+            //     );
+            //   },
+            // );
+            return Center(
+              child: ElevatedButton(
+                onPressed: () {
+                  setState(() {
+
+                  });
+                },
+                child: Text("Click"),
+              ),
             );
           }
           return const Center(
